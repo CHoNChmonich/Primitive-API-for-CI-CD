35f7c2f2ee9f09e77c888b3896200df89a3158a0
Artem <mihaylovartem54@gmail.com>
2024-12-10 05:11:22 +0300

    UserBookRelation model added

--- books/models.py
+++ books/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from pycparser.ply.yacc import default_lr
+
 LANGUAGE_CHOICES = {
     'en': 'English',
     'es': 'Spanish',
@@ -18,3 +20,19 @@ class Books(models.Model):
 
     def __str__(self):
         return f'{self.name} за авторством {self.author} | {self.id}'
+
+
+
+class UserBookRelation(models.Model):
+    RATE_CHOICES = {
+        '1': 1,
+        '2': 2,
+        '3': 3,
+        '4': 4,
+        '5': 5
+    }
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='related_book')
+    book = models.ForeignKey(Books, on_delete=models.CASCADE, related_name='related_user')
+    like = models.BooleanField(default=False)
+    in_bookmarks = models.BooleanField(default=False)
+    rate = models.PositiveSmallIntegerField(choices=RATE_CHOICES, default=1)
